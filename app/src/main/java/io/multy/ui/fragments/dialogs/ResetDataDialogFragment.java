606971656fe67a3da92329fa18ecfc8d4bd8f65a
ihar-paliashchuk <ihar.paliashchuk@gmail.com>
2018-01-24 10:45:45 +0300

    add reset dialog layout and fragment (#78)

---
+++ app/src/main/java/io/multy/ui/fragments/dialogs/ResetDataDialogFragment.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 Idealnaya rabota LLC
+ * Licensed under Multy.io license.
+ * See LICENSE for details
+ */
+
+package io.multy.ui.fragments.dialogs;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.multy.R;
+
+
+public class ResetDataDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_settings_reset, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+
+    @OnClick(R.id.button_positive)
+    public void onClickPositive() {
+
+    }
+
+    @OnClick(R.id.button_neutral)
+    public void onClickNeutral() {
+        dismiss();
+    }
+
+}
